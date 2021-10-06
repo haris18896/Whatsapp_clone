e1e90cd7d8f19b3ca24668dc5455b85464fd94d7
haris18896 <haris18896@gmail.com>
2021-10-06 06:48:42 -0700

    working with messages in the chat component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
-import './Chat.css'
+import './Chat.css';
+import { Avatar, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+
 function Chat() {
     return (
         <div className="chat">
-            <h1>Chat</h1>
+            <div className="chat__header">
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at ...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchIcon />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Haris</span>
+
+                    This is a message
+
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+
         </div>
     )
 }
+
 
 export default Chat
