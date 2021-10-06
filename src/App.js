e9b242a807907bff3e934cba35bf9ff0847a8488
haris18896 <haris18896@gmail.com>
2021-10-06 04:37:50 -0700

    initial components has been rendered

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css';
+import Chat from './components/chat/Chat';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="app">
-        <h1>clone</h1>
+        <Sidebar />
 
-        {/* Sidebar */}
-        {/* chatComponent */}
+        <Chat />
     </div>
   );
 }
