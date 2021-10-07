b5d55317184ff2ffb44b32ccbe38f0fb9e174ed5
haris18896 <haris18896@gmail.com>
2021-10-07 03:49:21 -0700

    messaging on alert are working in the App.js file

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # Whatsapp_clone
 
+## `Sync front end to backend`
 
+`npm i pusher-js`
 
+```js
+// /src/App.js
+function App() {
 
+  useEffect(() => {
+    const pusher = new Pusher('5656d95bb85395c320d8', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  },[]);
+```
 
 
 
--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React,{ useEffect } from 'react'
 import './App.css';
 import Chat from './components/chat/Chat';
 import Sidebar from './components/sidebar/Sidebar';
+import Pusher from 'pusher-js'
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher('5656d95bb85395c320d8', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  },[]);
+
   return (
     <div className="app">
       <div className="app__body">
