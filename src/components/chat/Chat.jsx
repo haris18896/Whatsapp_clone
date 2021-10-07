fb7f0fd2034318eda396ef9105126b11e1c3db72
haris18896 <haris18896@gmail.com>
2021-10-06 23:34:00 -0700

    chat added

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -4,6 +4,9 @@ import { Avatar, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import MicIcon from '@mui/icons-material/Mic';
+import SendIcon from '@mui/icons-material/Send';
 
 
 function Chat() {
@@ -33,31 +36,36 @@ function Chat() {
             <div className="chat__body">
                 <p className="chat__message">
                     <span className="chat__name">Haris</span>
-
                     This is a message
-
                     <span className="chat__timestamp">
                         {new Date().toUTCString()}
                     </span>
                 </p>
                 <p className="chat__message chat__receiver">
                     <span className="chat__name">Haris</span>
-
                     This is a message
-
                     <span className="chat__timestamp ">
                         {new Date().toUTCString()}
                     </span>
                 </p>
                 <p className="chat__message">
                     <span className="chat__name">Haris</span>
-
                     This is a message
-
                     <span className="chat__timestamp">
                         {new Date().toUTCString()}
                     </span>
                 </p>
+            </div>
+
+            <div className="chat__footer">
+                <EmojiEmotionsIcon />
+                <form>
+                    <input placeholder="Type a message" type="text" />
+                    <button type="submit">
+                        <SendIcon />
+                    </button>
+                    <MicIcon />
+                </form>
             </div>
 
         </div>
